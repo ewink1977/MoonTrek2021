296ecb10feca019b58421d115321db399c86fe69
Erin Winking <ewink@yahoo.com>
2021-02-08 00:24:54 -0800

    Base template w/NAVBAR, FOOTER built. Landing page, Contact page built.

--- MTBase/urls.py
+++ MTBase/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from MTBase import views
 
+app_name = 'base'
+
 urlpatterns = [
-    path('', views.IndexView, name = 'mainPage'),
+    path('', views.IndexView, name = 'landingPage'),
+    path('contact/', views.ContactView.as_view(), name = 'contactPage'),
 ]
--- MTBase/views.py
+++ MTBase/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 def IndexView(request):
     return render(request, 'html/baseIndex.html')
 
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'html/contact.html', {'pagetitle': 'Moon Trek | Contact ewink!'})
+    
+    def post(self, request):
+        pass
