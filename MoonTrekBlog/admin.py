63c4db2826a61eddf3c24b9033aa42f029eb88f7
Erin Winking <ewink@yahoo.com>
2021-02-09 14:37:05 -0800

    Fixed ADMIN error that was preventing SLUG from updating.

--- MoonTrekBlog/admin.py
+++ MoonTrekBlog/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from .models import BlogPost
 
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content',)
+    list_display = ('title', 'author',)
     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(BlogPost)
-
-
+admin.site.register(BlogPost, BlogPostAdmin)
