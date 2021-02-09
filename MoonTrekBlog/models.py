c9a32ab384a374a1ce64a8a99eb7ec00bc9215db
Erin Winking <ewink@yahoo.com>
2021-02-08 20:33:04 -0800

    Created Model for Blog Posts, removed dummy data.

--- MoonTrekBlog/models.py
+++ MoonTrekBlog/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
-    pass
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    # slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    author = models.ForeignKey(
+        User, 
+        on_delete = models.CASCADE
+    )
+
+
--- MoonTrekStories/views.py
+++ MoonTrekStories/views.py
@@ -1,27 +1,9 @@
 from django.shortcuts import render, redirect 
-
-# Dummy Data For Formatting.
-# VVVVVVVVVVVVVVVVVVVVVVVVVV
-nyaaBlog = [
-    {
-        'author': 'ewink',
-        'title': 'This is a blog post!',
-        'content': 'This is test content! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, explicabo eius illum incidunt et laudantium labore voluptatibus! Nobis consectetur dignissimos sit praesentium non quia, eum facilis vitae doloribus. Cum, in?',
-        'date_posted': 'January 31, 2021'
-    },
-    {
-        'author': 'ewink',
-        'title': 'A Second Post!',
-        'content': 'The first 50 words or so... Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, explicabo eius illum incidunt et laudantium labore voluptatibus! Nobis consectetur dignissimos sit praesentium non quia, eum facilis vitae doloribus. Cum, in?',
-        'date_posted': 'January 31, 2021'
-    }
-]
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# End Dummy Data
+from MoonTrekBlog.models import BlogPost
 
 def storyHome(request):
     context = {
-        'posts': nyaaBlog,
+        'posts': BlogPost.objects.all(),
         'pagetitle': 'Moon Trek | Stories Home Page'
     }
     return render(request, 'html/storyHome.html', context)
