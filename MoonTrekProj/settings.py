9303fba8dfe098a1f339a8355193260e1edc81c1
Erin Winking <ewink@yahoo.com>
2021-03-01 00:18:46 -0800

    Closes #1. Added  statement to  which uses .

--- MoonTrekProj/settings.py
+++ MoonTrekProj/settings.py
@@ -144,6 +144,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# A redirect to redirect the only user of the site... ME!
+# A redirect to redirect the only user of the site that can login... ME!
 LOGIN_REDIRECT_URL = 'stories:storyHome'
 
