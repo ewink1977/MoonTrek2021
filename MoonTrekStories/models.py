06540e8d06205cfbda157213196d0f6f4c3a3744
Erin Winking <ewink@yahoo.com>
2021-03-04 03:11:55 -0800

    Closes #21 by adding POSITION INT field to MTS model. Added EPILOGUE boolean field to MTC model.

--- MoonTrekStories/models.py
+++ MoonTrekStories/models.py
@@ -7,6 +7,7 @@ class MoonTrekStories(models.Model):
     image = models.ImageField(default = 'placeholder.jpg',
         upload_to = 'storyIMG')
     series = models.IntegerField()
+    position = models.IntegerField()
     summary = HTMLField()
     description = models.CharField(max_length = 255, default = "Please replace me.")
     slug = models.SlugField()
@@ -21,13 +22,14 @@ class MoonTrekChapters(models.Model):
     chapter_number = models.IntegerField()
     title = models.CharField(max_length = 255)
     content = HTMLField()
+    epilogue = models.BooleanField(default = False)
     slug = models.SlugField()
     story = models.ForeignKey(MoonTrekStories, 
         related_name = 'chapters', 
         on_delete = models.CASCADE)
 
     def __str__(self):
-        return f"Chapter {self.chapter_number} - {self.title}"
+        return f"{self.story.title} : Chapter {self.chapter_number} - {self.title}"
 
     def get_absolute_url(self):
-        return reverse('chapterPage', kwargs={'slug': self.slug})
+        return reverse('chapterPage', kwargs={'slug': self.slug, 'story_slug': self.story.slug})
