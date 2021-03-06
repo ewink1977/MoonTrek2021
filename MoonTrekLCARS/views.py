3e70a5b7ba407354e35dc70de8529d212726d1a1
Erin Winking <ewink@yahoo.com>
2021-03-05 16:34:44 -0800

    LCARS Homepage templated to display link to subsection ListView Pages.

--- MoonTrekLCARS/views.py
+++ MoonTrekLCARS/views.py
@@ -1,27 +1,33 @@
 import random
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from MoonTrekLCARS.models import Character, Ship, PlacesAndItems
 
-def buffer(request):
-    return render(request, 'html/baseIndex.html')
-
-class LCARSHome(ListView):
-    context_object_name = 'character_list'
-    model = Character
+class LCARSHome(TemplateView):
     template_name = 'MoonTrekLCARS/homepage.html'
 
     def get_context_data(self, **kwargs):
         context = super(LCARSHome, self).get_context_data(**kwargs)
-        context['ship_list'] = Ship.objects.all()
-        context['place_item_list'] = PlacesAndItems.objects.all()
         context['pagetitle'] = 'Moon Trek | LCARS Database'
+        # PUT THE RANDOM ARTICLE CODE HERE.
 
         return context
 
+    # FUNCTION FOR FINDING A RANDOM ARTICLE TO DISPLAY!
     def randomArticle():
         fromModel = random.randint(1, 3)
-        print(fromModel)
-
+        if fromModel == 1:
+            last = Character.objects.all().count() - 1
+            randIndex = random.randint(1, last)
+            randomArticle = Character.objects.get(id = randIndex)
+        if fromModel == 2:
+            last = Ship.objects.all().count() - 1
+            randIndex = random.randint(1, last)
+            randomArticle = Ship.objects.get(id = randIndex)
+        if fromModel == 3:
+            last = PlacesAndItems.objects.all().count() - 1
+            randIndex = random.randint(1, last)
+            randomArticle = PlacesAndItems.objects.get(id = randIndex)
+        return randomArticle
 
 # <app>/<model>_<type>.html
