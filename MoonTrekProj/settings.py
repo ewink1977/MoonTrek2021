bab45b0982abd4fb8c1b985b82d07045cfef661b
Erin Winking <erin@douglasavenue.com>
2021-04-25 01:57:35 -0700

    Minor settings edit for alpha deployment.

--- MoonTrekProj/settings.py
+++ MoonTrekProj/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['143.198.101.249']
 
 
 # Application definition
