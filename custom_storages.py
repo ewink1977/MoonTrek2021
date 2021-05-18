f444e2d469be3c74be0216a587feafae8e19ff23
Erin Winking <erin@douglasavenue.com>
2021-05-18 00:22:34 -0700

    Resolved #31, Resolved #32. Removed unnecessary Fontawesome CSS STATIC link.

--- custom_storages.py
+++ custom_storages.py
@@ -3,7 +3,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-
+    default_acl = 'public-read'
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
     default_acl = 'public-read'
