926a5b1b849167ac8e899f02a62d5b6533d8ad5b
Erin Winking <erin@douglasavenue.com>
2021-09-03 01:47:06 -0700

    Final commit before live. Minor template changes.

--- MoonTrekProj/settings.py
+++ MoonTrekProj/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['143.198.101.249', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['moontrek.douglasavenue.com', '143.198.101.249', 'localhost', '127.0.0.1']
 
 
 # Application definition
